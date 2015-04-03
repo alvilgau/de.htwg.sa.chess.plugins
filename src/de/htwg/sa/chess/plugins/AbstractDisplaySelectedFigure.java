1c26d00bce6a7b949a735f74aef2a1eec4161da9
Artur Kostka <Artur.Kostka@gmail.com>
2015-04-03 12:24:23 +0200

    did some implementations of plugins

---
+++ src/de/htwg/sa/chess/plugins/AbstractDisplaySelectedFigure.java
@@ -0,0 +1,15 @@
+package de.htwg.sa.chess.plugins;
+
+/**
+ * Abstract class, which provides methods for figure display.
+ * 
+ * @author arkostka
+ * @since 2015-04-03
+ */
+public class AbstractDisplaySelectedFigure {
+
+	public String getCurrentFigurePosition() {
+		return "i'm here bitch";
+	}
+
+}
---
+++ src/de/htwg/sa/chess/plugins/TextualDisplaySelectedFigure.java
@@ -0,0 +1,18 @@
+package de.htwg.sa.chess.plugins;
+
+import de.htwg.chess.plugin.selectedfigure.IDisplaySelectedFigurePlugin;
+
+/**
+ * @author arkostka
+ * @since 2015-04-03
+ *
+ */
+public class TextualDisplaySelectedFigure extends
+		AbstractDisplaySelectedFigure implements
+		IDisplaySelectedFigurePlugin {
+
+	@Override
+	public void displayFigure() {
+
+	}
+}
