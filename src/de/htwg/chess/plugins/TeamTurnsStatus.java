e3c000ef61a2b4cdef34e4bcc2f49065871ffbdd
Artur Kostka <Artur.Kostka@gmail.com>
2015-04-04 22:07:44 +0200

    Added plugin to display amount of turns by team

--- src/de/htwg/chess/plugins/TeamTurnsStatus.java
+++ src/de/htwg/chess/plugins/TeamTurnsStatus.java
@@ -8,19 +8,15 @@ import javax.swing.JLabel;
 import org.apache.log4j.Logger;
 
 import de.htwg.chess.controller.IChessController;
-import de.htwg.chess.model.IFigure;
-import de.htwg.chess.plugins.StatusPlugin;
 import de.htwg.util.observer.Event;
 
-public class SelectedFigureStatus implements StatusPlugin {
-
-	private static final String COLS = "ABCDEFGH";
+public class TeamTurnsStatus implements StatusPlugin {
 
 	private Logger logger = Logger.getLogger(getClass());
 	private JLabel component;
 	private IChessController controller;
-
-	public SelectedFigureStatus() {
+	
+	public TeamTurnsStatus() {
 		this.component = new JLabel();
 		this.component.setFont(new Font("Arial", Font.CENTER_BASELINE, 16));
 	}
@@ -34,38 +30,31 @@ public class SelectedFigureStatus implements StatusPlugin {
 
 	@Override
 	public void stop() {
-		this.logger.info("Disabled Plugin: " + getName());
+		this.logger.info("Disabled Plugin: " + getName());	
 	}
 
 	@Override
 	public String getName() {
-		return "Display Selected Figure";
+		return "Display total turns for team";
 	}
 
 	@Override
 	public JComponent getComponent() {
 		return this.component;
 	}
-
+	
 	private String getData() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(" Selected Figure: ");
-		if (this.controller.isSelect()) {
-			IFigure figure = this.controller.getSelectedFigure();
-			int xPos = figure.getxPos();
-			int yPos = figure.getyPos() + 1;
-			sb.append(figure.getTeam() + " ");
-			sb.append(figure.getClass().getSimpleName());
-			sb.append(" [" + COLS.charAt(xPos));
-			sb.append(yPos + "]");
-		} else {
-			sb.append("No figure");
-		}
+		sb.append(" Total turns white: " + this.controller.getTurnsWhite());
+		sb.append(", \n");
+		sb.append(" total turns black: " + this.controller.getTurnsBlack());
 		return sb.toString();
 	}
-
+	
 	@Override
 	public void update(Event e) {
 		this.component.setText(getData());
+		
 	}
+
 }
